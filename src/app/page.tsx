33ab899e030f639b1be0e17166c3854652279f69
nifnip22 <proxigaming742@gmail.com>
2025-06-14 15:19:29 +0800

    feat: hero section (not responsive)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { GridBackgroundDemo } from "@/components/GridBackground";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="w-full font-[family-name:var(--font-jakarta-sans)]">
       <Navbar />
+      <GridBackgroundDemo />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-between items-center p-4">
+    <nav className="fixed top-0 left-0 right-0 z-50 w-full flex justify-between items-center p-4">
       <div className="flex items-center gap-x-1">
         <Image src={"/icons/favicon.png"} alt="GreenSpace Logo" width={32} height={32} className="w-10" />
         <h1 className="font-bold text-xl">GreenSpace</h1>
